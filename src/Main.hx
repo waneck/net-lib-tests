7937582e088af75e6e5c820fa2ac60866aa48cd2
Cauê Waneck <waneck@gmail.com>
2014-03-08 22:55:05 -0300

    cairo test

--- src/Main.hx
+++ src/Main.hx
@@ -1,10 +1,14 @@
 import unityengine.*;
 import unityeditor.*;
+import cairo.ImageSurface;
+import cairo.*;
 class Main
 {
 
 	static function main()
   {
+		var surface = new ImageSurface(Format.RGB24, 120, 120);
+		var cr = new Context(surface);
 	}
 
 }
